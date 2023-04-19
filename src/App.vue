905da7cc9a0811f7f1214650650dade6804acc9d
yaredghebre <yared2391@gmail.com>
2023-04-19 07:22:24 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      store
+    }
+  },
+}
+</script>
+
+<template>
+
+</template>
+
+<style>
+</style>
